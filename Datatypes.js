4b3f7f98056636f30dc835871e7cfc9ae180990c
vedant Kale <vedantkale0707@gmail.com>
2025-04-30 14:47:46 +0530

    add datatype of js file

---
+++ Datatypes.js
@@ -0,0 +1,30 @@
+console.log("integer");
+
+// primitive types 
+// 1. number 
+// 2. string
+// 3. boolean
+// 4. undefined 
+// 5. null
+// 6. big int  2**53-1
+// 7. symbol
+
+console.log("-----Number data type-----");
+marks =22.22
+console.log("marks:"+marks);
+
+console.log("---boolean");
+a=true;
+console.log(a);
+
+console.log("---undefined------");
+let aa;
+console.log(aa);
+
+console.log("--- null data type----------");
+let value=null;
+ console.log(value);
+
+ // find a which type of datatype 
+ let no="vedant"
+ console.log(typeof(no));
