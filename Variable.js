46629cd51e730237ad2e74bf0df6c50edae235c2
vedant Kale <vedantkale0707@gmail.com>
2025-04-30 14:32:09 +0530

    variable program is added

---
+++ Variable.js
@@ -0,0 +1,37 @@
+
+console.log("declare a variable using let"); 
+let a=10;
+console.log(a);
+
+// /// double declaration is not allowed in js 
+// // let b=10;
+// // let b=20;
+
+// // change in value is allow
+
+console.log("-----change value of varibale");
+let c=10;
+console.log(c);
+ c=22;
+ console.log(c);
+
+//  let v=11;
+//  v='vedant';
+//  console.log(v);
+
+
+ // using const 
+// we cannot change value of const variable 
+console.log("------variable declared using const");
+ const x=11;
+ console.log(x);
+//  x=12;
+//  console.log(x);
+
+// variable declared using var 
+
+console.log("-----varibale declared using var keywork----");
+var aa=10;
+console.log(aa);
+
+
