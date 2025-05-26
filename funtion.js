84b00d463f1dc37e8e10a09dad610f386d6f39d0
vedant Kale <vedantkale0707@gmail.com>
2025-05-26 19:14:34 +0530

    funtions file is added

---
+++ funtion.js
@@ -0,0 +1,22 @@
+function addtion(num1, num2){
+    let result=num1+num2
+    console.log("result:",result)
+}
+
+
+function evenOdd(num){
+    if(num%2===0)
+        console.log(`${num} is even`)
+    else
+        console.log(`${num} is odd`)
+    
+}
+
+function multiplication(no1, no2){
+    let result=no1*no2
+    console.log("multiplication:",result)
+}
+
+addtion(23,23)
+evenOdd(11)
+multiplication(12,12)
