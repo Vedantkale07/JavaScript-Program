0ff0526ae228df2036bf813df8803b2653028bb3
vedant Kale <vedantkale0707@gmail.com>
2025-06-25 18:24:29 +0530

    arrays and string program is added

---
+++ StringArrayObject/array.js
@@ -0,0 +1,28 @@
+
+let arr=[3,4,5, 11, 14,11, "ved", 8,9]
+arr.slice(1,4);
+console.log(arr)
+
+arr.push(111);
+console.log(arr)
+
+// arr.pop();
+// console.log(arr)
+// arr.shift();
+
+console.log(arr)
+
+// splice will return the element which have delete [splice(index, numOfDel)]
+// let reverse =arr.splice(3,3, 99,199)
+// console.log(arr)
+// console.log(reverse); 
+
+let fruit= ["mango", "apple","strowberry"];
+// console.log(fruit)
+// console.log(typeof(fruit))
+
+let test =fruit.join()
+console.log(test);
+console.log(typeof test) 
+
+
---
+++ StringArrayObject/string.js
@@ -0,0 +1,53 @@
+// // console.log("veant kale");
+// // console.log('vedant kale');
+
+// let name='vedantkale'
+// let str=`vedant kale `
+// console.log(str, typeof str)
+// console.log(name, typeof name)
+ 
+// firstname='vedant'
+// middlename='balshiram'
+// lastname="kale"
+
+// let fullname= firstname+" "+middlename+" "+lastname
+// console.log(fullname)
+
+// fullname2=`${firstname} ${middlename} ${lastname}`
+// console.log(fullname2);
+
+// console.log(firstname, firstname.length);
+// console.log(middlename, middlename.length);
+
+// let str="vedant";
+// console.log(str, str.length)
+
+// console.log(str[0])
+
+// for (const element of str) {
+//     console.log(element)
+// }
+
+// console.log("use for in loop it will give a index value")
+
+// for (const key in str) {
+//     console.log(key)
+// }
+
+let str2= "im a programmer ";
+
+console.log(str2.includes("fs"))
+
+console.log(str2.charAt(3));
+
+console.log(str2.replace("programmer","coder"));
+
+console.log(str2.slice(-11,12 ))
+
+console.log(str2.substring(5,15))
+
+// let fullnam=prompt("enter your full name:");
+// console.log(fullnam)
+
+let name= "hello im a boy"
+console.log(name.split(" "));
