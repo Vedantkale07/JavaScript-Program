67457d5d93de2b67e484fa7084bc54d92fa9791b
vedant Kale <vedantkale0707@gmail.com>
2025-06-26 21:32:47 +0530

    arrays in js file is addded

---
+++ StringArrayObject/Array_2.js
@@ -0,0 +1,27 @@
+// let str="markram";
+//   let str2= str.split("").reverse().join("");
+//   console.log(str2)
+
+//   if (str2=== str) {
+//     console.log(` ${str} is palindrom string`)
+//   } else {
+//     console.log(`${str} is not palindrom string`)
+//   }
+
+
+// let str= "how are you dear"
+
+// let str2=str.split(" ").map((ele)=>{
+//     return (ele.charAt(0).toUpperCase() + ele.slice(1))
+// })
+// console.log(str2.join(" "))
+
+
+
+let nam= "my name is vedant"
+
+ let nam2= nam.split(" ").map((ele)=>{
+    return (ele.charAt(0).toUpperCase() + ele.slice(1));
+
+ })
+ console.log(nam2.join(" "))
