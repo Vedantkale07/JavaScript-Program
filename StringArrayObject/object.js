f0a2280db9bdf25b31af42220fc74d4bda433e0d
vedant Kale <vedantkale0707@gmail.com>
2025-06-28 20:48:43 +0530

    object in js program file are added

--- StringArrayObject/object.js
+++ StringArrayObject/object.js
@@ -8,3 +8,28 @@ let car ={
 }
 
 console.log(car.start())
+
+let obj={
+    name:"vedant",
+    age:23,
+    weight:"50kg"
+}
+
+let key=Object.keys(obj)
+console.log(key)
+
+let value= Object.values(obj)
+console.log(value)
+
+for (const key in obj) {
+    console.log(key)
+}
+
+let entries = Object.entries(obj)
+console.log(entries)
+
+
+obj2={
+    ...obj
+}
+console.log(obj2)
