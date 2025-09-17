1f03d609433c93afd603395cd1e892ae5096303f
vedant Kale <vedantkale0707@gmail.com>
2025-09-17 20:36:22 +0530

    add 1 program

--- ArrowFunction.js
+++ ArrowFunction.js
@@ -1,17 +1,17 @@
 // <<<<<<< HEAD
-// arrow function syntax
+// arrow function syntax  
 () =>{}
 
 const addTwo=(no1, no2) => {
     return no1+no2
 }
 const result=addTwo(2,2)
-console.log(result)
+console.log(result);
 
 //implisite return : means dont need to write return keyword its do itself
 const addtion=(no1,no2)=> no1+no2
 
-console.log(addtion(3,3))
+console.log(addtion(3,3));
 
 // when you try to return object it should be in the parenthesis 
 
