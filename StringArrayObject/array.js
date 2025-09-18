5e3a1c4d0327f41c377219234fbcdf65be5f8dfb
vedant Kale <vedantkale0707@gmail.com>
2025-09-18 18:04:37 +0530

    program is added

--- StringArrayObject/array.js
+++ StringArrayObject/array.js
@@ -1,28 +1,32 @@
-
-let arr=[3,4,5, 11, 14,11, "ved", 8,9]
-arr.slice(1,4);
-console.log(arr)
+let arr = [3, 4, 5, 11, 14, 11, "ved", 8, 9];
+arr.slice(1, 4);
+console.log(arr);
 
 arr.push(111);
-console.log(arr)
+console.log(arr);
 
 // arr.pop();
 // console.log(arr)
 // arr.shift();
 
-console.log(arr)
+console.log(arr);
 
 // splice will return the element which have delete [splice(index, numOfDel)]
 // let reverse =arr.splice(3,3, 99,199)
 // console.log(arr)
-// console.log(reverse); 
+// console.log(reverse);
 
-let fruit= ["mango", "apple","strowberry"];
+let fruit = ["mango", "apple", "strowberry", "litchi"];
 // console.log(fruit)
 // console.log(typeof(fruit))
 
-let test =fruit.join()
+let test = fruit.join(" & ");
 console.log(test);
-console.log(typeof test) 
+console.log(typeof test);
 
+// find method example
+let ans = fruit.find(function (item) {
+  return item === "mango";
+});
 
+console.log(ans);
