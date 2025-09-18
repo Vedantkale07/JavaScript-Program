dca14df2d6497942dda864f8fc3da0384b77192b
vedant Kale <vedantkale0707@gmail.com>
2025-09-18 18:01:33 +0530

    program is added

--- reduce.js
+++ reduce.js
@@ -1,4 +1,4 @@
-const arr= [1,2,3,4];
+const arr = [1, 2, 3, 4];
 
 // const total= arr.reduce(function (accumulator, currentValue) {
 //         console.log(`accumulator: ${accumulator} currentValue: ${currentValue}`);
@@ -11,24 +11,24 @@ const arr= [1,2,3,4];
 // const noTotal= arr.reduce((acc, current)=>(acc+current));
 // console.log(noTotal);
 
-const shopping =[
-    {
-        item:'java',
-        price:2999
-    },
-    {
-        item:'javaScript',
-        price:4999
-    },
-    {
-        item:'python',
-        price:9999
-    },
-    {
-        item:'SAP',
-        price:12999
-    }
-]
+const shopping = [
+  {
+    item: "java",
+    price: 2999,
+  },
+  {
+    item: "javaScript",
+    price: 4999,
+  },
+  {
+    item: "python",
+    price: 9999,
+  },
+  {
+    item: "SAP",
+    price: 12000,
+  },
+];
 
-const totalPrice= shopping.reduce((acc, item)=> (acc+item.price),0);
+const totalPrice = shopping.reduce((acc, item) => acc + item.price, 0);
 console.log(totalPrice);
